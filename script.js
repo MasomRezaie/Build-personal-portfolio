f2147e0d0f48d023b7057c7239d4182aa1a73062
MasomRezaie <masom.rezaie135@gmail.com>
2024-08-03 21:07:47 -0700

    added an eventlistnere

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
